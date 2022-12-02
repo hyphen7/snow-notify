0d803c004a147feb6056111728d19858b83ad7cc
hyphen7 <akikawarenon@gmail.com>
2022-12-02 06:31:25 +0000

    /hello 追加

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
     res.send("<h1>Hi! There.<h1>");
 });
 
+app.get('/hello', (req, res) => {
+    const name = req.query.name || "anonymous";
+    res.send(`Hello ${name}!`);
+});
+
 app.listen(PORT, () => {
     console.log("Express is runnning");
 });
