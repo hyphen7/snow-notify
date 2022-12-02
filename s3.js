ba03ca8c1833a1ed594a11761288977f9b064146
hyphen7 <akikawarenon@gmail.com>
2022-12-02 07:50:03 +0000

    s3へのアクセス確認

---
+++ s3.js
@@ -0,0 +1,20 @@
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
+
+async function s3obj() {
+    // store something
+    await s3.putObject({
+        Body: JSON.stringify({ key: "value?" }),
+        Bucket: "cyclic-gleaming-wrap-hare-us-west-2",
+        Key: "some_files/my_file.json",
+    }).promise();
+
+    // get it back
+    let my_file = await s3.getObject({
+        Bucket: "cyclic-gleaming-wrap-hare-us-west-2",
+        Key: "some_files/my_file.json",
+    }).promise();
+
+    console.log(JSON.parse(my_file.Body.toString()));
+}
+s3obj();
