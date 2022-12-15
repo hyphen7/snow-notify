74cf3c09cf0e9d59e02f628fa69e3710e2348c9d
hyphen7 <akikawarenon@gmail.com>
2022-12-15 03:05:02 +0000

    処理実行環境整備

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const Express = require('express');
+const task = require('./task.js');
 const app = Express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send("<h1>Hi! There.<h1>");
+//タスク実行認証チェック
+app.use((req, res, next) => {
+    const EXEC_KEY = process.env.EXEC_KEY;
+    const key = req.headers['x-exec-key'];
+    if (key == EXEC_KEY) {
+        next();
+    } else {
+        console.log(`Unauthorized Request - ${key}`);
+        res.status(401).json({ message: "Unauthorized" });
+    }
 });
 
-app.get('/hello', (req, res) => {
-    const name = req.query.name || "anonymous";
-    res.send(`Hello ${name}!`);
+//タスク本処理
+app.get('/', (req, res, next) => {
+    console.log("Start Task");
+    task().then(() => {
+        console.log("End Task");
+        res.sendStatus(200);
+    }).catch(e => {
+        next(e);
+    });
+});
+
+//タスク異常終了時の500エラー
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).json({ message: "Internal Server Error", details: err });
 });
 
 app.listen(PORT, () => {
